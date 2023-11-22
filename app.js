c879879005b7d34c9abcc3b73145685320fa0832
정윤서 <91608021+Avoler0@users.noreply.github.com>
2023-11-22 19:57:13 +0900

    Revert "이메일 인증 테스트"

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-import express from 'express';
-import emailRouter from "./src/routers/emailtest.router.js";
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 
-app.use('/api',emailRouter);
+app.use(express.json());
 
 app.get('/',(req,res)=>{
   res.send('Hello World !')
