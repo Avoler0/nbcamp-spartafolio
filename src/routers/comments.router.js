4ff8804f87be810fa04d4610bea40e3df60ed011
Junsik <chzhgod@gmail.com>
2023-11-22 17:40:05 +0900

    feat: [reply] 댓글 삭제기능 추가
    - 본인이 쓴 댓글이 아닐 경우 삭제는 추후 추가 예정

--- src/routers/comments.router.js
+++ src/routers/comments.router.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import createComments from '../controller/comments/create.js';
-import readComments from '../controller/comments/read.js';
-import updateComment from '../controller/comments/update.js';
+import createComments from '../controller/comments/create.js'; // 댓글 추가 API
+import readComments from '../controller/comments/read.js'; // 댓글 가져오기 API
+import updateComment from '../controller/comments/update.js'; // 댓글 수정 API
+import deleteComment from '../controller/comments/delete.js'; // 댓글 삭제 API
+
 const commentsRouter = express.Router();
 
 // 댓글 달기 API
@@ -10,7 +12,10 @@ commentsRouter.post("/comment", createComments);
 // 댓글 가져오기 API
 commentsRouter.get('/:projectId/comments', readComments);
 
-// 댓글 수정하기 API
-commentsRouter.put('/comment/:commentId', updateComment)
+// 댓글 수정하기
+commentsRouter.put('/comment/:commentId', updateComment, deleteComment);
+
+// 댓글 삭제하기
+commentsRouter.delete('/comment/:commentId', deleteComment);
 
 export default commentsRouter;
