6a5ab259fe3fc9f155f50360f02305cf7b5dd952
Junsik <chzhgod@gmail.com>
2023-11-24 16:34:48 +0900

    feat: [project-detail] 각 프로젝트 별 댓글 달기 기능 추가

--- src/routers/projects.router.js
+++ src/routers/projects.router.js
@@ -17,13 +17,13 @@ projectRouter.post('/post', upload.array('additional'), async (req, res) => {
     try {
         const existingUser = await Users.findByPk(1);
         const project = await Projects.create({
-            title:projectTitle,
-            team_name:teamName,
-            over_view:overView,
-            tech_stack:techStack,
-            github_address:githubAddress,
-            core_function:coreFunction,
-            demo_site:demoSite,
+            title: projectTitle,
+            team_name: teamName,
+            over_view: overView,
+            tech_stack: techStack,
+            github_address: githubAddress,
+            core_function: coreFunction,
+            demo_site: demoSite,
             description, user_id: existingUser.user_id,
             images_path: filePath.join(",")
         });
@@ -38,19 +38,19 @@ projectRouter.post('/post', upload.array('additional'), async (req, res) => {
 projectRouter.get('/posts', async (req, res) => {
     try {
         const projects = await Projects.findAll({
-          attributes: [
-            'project_id',
-            'title',
-            'team_name',
-            'github_address',
-            'images_path',
-            'tech_stack',
-            'over_view',
-            'like',
-            'view',
-            'createdAt'
-          ],
-          order: [['createdAt', 'desc']],
+            attributes: [
+                'project_id',
+                'title',
+                'team_name',
+                'github_address',
+                'images_path',
+                'tech_stack',
+                'over_view',
+                'like',
+                'view',
+                'createdAt'
+            ],
+            order: [['createdAt', 'desc']],
         });
         res.status(200).json({ message: "게시물 조회", projects });
     } catch (error) {
