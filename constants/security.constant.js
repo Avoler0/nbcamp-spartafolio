a0770c991ca0b35c1022750d90c6c07e1f0e35c8
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-11-24 19:01:57 +0900

    feat: 로그인, 회원가입 화면 수정(gif, css 추가)

--- constants/security.constant.js
+++ constants/security.constant.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export const PASSWORD_HASH_SALT_ROUNDS = Number.parseInt(
   process.env.PASSWORD_HASH_SALT_ROUNDS,
-  10,
+  15,
 );
 
 export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
